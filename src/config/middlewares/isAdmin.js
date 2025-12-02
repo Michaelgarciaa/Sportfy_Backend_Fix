6a6d203f92588fe9d20ddd369d3e562bc01715eb
Michaelgarciaa <michaelgrcaa@gmail.com>
2025-12-03 00:06:22 +0700

    https://github.com/Deofajar10/Backend_Sportfy_Deo_Michael_Jeco.git

--- src/config/middlewares/isAdmin.js
+++
@@ -1,7 +0,0 @@
-module.exports = function isAdmin(req, res, next) {
-  const role = req.user?.role;
-  if (role !== 'ADMIN') {
-    return res.status(403).json({ success: false, message: 'Hanya admin yang boleh mengakses.' });
-  }
-  next();
-};
